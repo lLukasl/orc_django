1a0f259dd08bb5820f8e305789b60a8b578a0d2d
lLukasl <llukasdiasl@hotmail.com>
2024-09-16 19:28:40 -0300

    adicionados model, urls e templates das paginas crm(cliente) e servico

--- backend/urls.py
+++ backend/urls.py
@@ -19,8 +19,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('backend.core.urls', namespace='core')),
-    #path('crm', include('backend.crm,urls', namespace='core')),
-    #path('orcamento', include('backend.orcamento,urls', namespace='core')),
-    #path('servico', include('backend.servico,urls', namespace='core')),
+    #path('crm', include('backend.crm.urls', namespace='crm')),
+    #path('orcamento', include('backend.orcamento.urls', namespace='orcamento')),
+    path('servico', include('backend.servico.urls', namespace='servico')),
     path('admin/', admin.site.urls),
 ]
